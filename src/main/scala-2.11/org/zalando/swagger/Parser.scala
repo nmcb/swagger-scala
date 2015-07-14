96a9c25525be700c978b91da27fdf94ba97f340f
nmcb <marco.borst@splatter.nl>
2015-07-14 10:46:07 +0200

    factored out json/yaml parsing

--- src/main/scala-2.11/org/zalando/swagger/Parser.scala
+++ src/main/scala-2.11/org/zalando/swagger/Parser.scala
@@ -2,14 +2,28 @@ package org.zalando.swagger
 
 import java.io.File
 
+import com.fasterxml.jackson.core.JsonFactory
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
-object Parser {
-  val factory = new com.fasterxml.jackson.dataformat.yaml.YAMLFactory()
+trait Parser {
+  def parse(file: File): Swagger
+}
+
+private[swagger] abstract class SwaggerParser extends Parser {
+  def factory: JsonFactory
   def parse(file: File): Swagger = {
     val mapper = new ObjectMapper(factory)
     mapper.registerModule(DefaultScalaModule)
     mapper.readValue(file, classOf[Swagger])
   }
+}
+
+object YamlParser extends SwaggerParser {
+  val factory = new YAMLFactory()
+}
+
+object JsonParser extends SwaggerParser {
+  val factory = new JsonFactory()
 }
--- src/test/scala-2.11/org/zalando/swagger/ParserTest.scala
+++ src/test/scala-2.11/org/zalando/swagger/ParserTest.scala
@@ -8,10 +8,17 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class ParserTest extends FlatSpec with Matchers {
-  
-  val fixture = new File("src/test/resources/uber.api.yaml")
 
-  "The swagger parser" should "read the uber specification" in {
-    val swagger = Parser.parse(fixture) 
+  val fixtures = Seq(
+    new File("src/test/resources/uber.api.json"),
+    new File("src/test/resources/uber.api.yaml")
+  )
+
+  "The json swagger parser" should "read swagger json specifications" in {
+    fixtures.filter(_.getName.endsWith(".json")).map(JsonParser.parse)
+  }
+
+  "The yaml swagger parser" should "read swagger yaml specifications" in {
+    fixtures.filter(_.getName.endsWith(".yaml")).map(YamlParser.parse)
   }
 }
